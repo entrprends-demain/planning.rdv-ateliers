bc6bb850909d7cb502587900dcc92649b0fe562f
entreprends-demain <entreprends.demain@paris-initiative.org>
2026-06-08 15:37:46 +0200

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* ── RDV Entreprends Demain · app.js ── */
+/* ── RDV Entreprends Demain · app.js ── */ 
 
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js';
 import { getFirestore,
